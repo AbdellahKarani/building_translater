09eb73b9da561e7fd57dc8233315ddca270a36ef
Abdellah <abdellahkarani@gmail.com>
2024-04-04 18:54:59 +0000

    .

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 // Start Translation Using Google API
-document.getElementById('button').addEventListener('click', translate);
 function translate() {
     var sourceText = document.querySelector('textarea#sourceText').value;
     var sourceLang = 'en';
     var targetLang = 'ar';
-
-    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
-
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" 
+    + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
     fetch(url)
         .then(response => response.json())
         .then(data => {
         document.querySelector('textarea#resultText').value = data[0][0][0];
-        console.log(data[0][0][0]);
         })
         .catch(error => console.error('Error:', error));
 }
+document.getElementById('button').addEventListener('click', function() {
+    var sourceText = document.getElementById('sourceText');
+    sourceText.value = sourceText.value.split("").filter(function(ele) {
+        return /^[a-zA-Z0-9 ]$/.test(ele); // Allow alphabets, numbers, and space
+    }).join("");
+    console.log(sourceText.value);
+    translate();
+});
+
 
 
 
