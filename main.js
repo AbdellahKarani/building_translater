7a07023a9c62e25527bcf37abd8f4debe26fe892
Abdellah <abdellahkarani@gmail.com>
2024-04-05 08:02:10 +0000

    .

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-// Start Translation Using Google API
 function translate() {
-    var sourceText = document.querySelector('textarea#sourceText').value;
-    var sourceLang = 'en';
-    var targetLang = 'ar';
-    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" 
-    + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    var sourceText = document.getElementById('sourceText').value;
+    var sourceLang = document.getElementById('sourceLang').value;
+    var targetLang = document.getElementById('targetLang').value;
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="+ sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
     fetch(url)
         .then(response => response.json())
         .then(data => {
-        document.querySelector('textarea#resultText').value = data[0][0][0];
+            document.getElementById('resultText').value = data[0][0][0];
         })
         .catch(error => console.error('Error:', error));
 }
-document.getElementById('button').addEventListener('click', function() {
-    var sourceText = document.getElementById('sourceText');
-    sourceText.value = sourceText.value.split("").filter(function(ele) {
-        return /^[a-zA-Z0-9 ]$/.test(ele); // Allow alphabets, numbers, and space
-    }).join("");
-    console.log(sourceText.value);
+var source = document.querySelector("#sourceText");
+var result = document.querySelector("#resultText");
+source.oninput = function() {
     translate();
+}
+source.addEventListener("focus", function() {
+    result.disabled = false;
 });
-
+source.addEventListener("mouseleave", function() {
+    result.disabled = true;
+});
 
 
 
