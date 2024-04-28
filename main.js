36ae138684fcd13ae7a598fb4bffe51b13c30a5a
Abdellah <abdellahkarani@gmail.com>
2024-04-28 21:42:59 +0100

    .

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// This The First Version
 function translate() {
     var sourceText = document.getElementById('sourceText').value;
     var sourceLang = document.getElementById('sourceLang').value;
